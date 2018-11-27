a5368c2e225f299a6d7db86693d7c07089c7daf4
Dillorom <dabdulloeva@gmail.com>
2018-11-27 14:07:25 -0800

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  namespace :api do 
-    resources :books, except: [:new, :edit]
-    #resources :users, only: [:show, :update, :create]
-  end 
-
+ 
   scope '/api' do
+    resources :books, except: [:new, :edit]
     resources :users
     post 'user_token' => 'user_token#create'
   end
